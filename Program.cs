8e2f907936827028f88df8f02acd1ade3e788c56
AnaPaulaVeloso <velosoanapaula29@gmail.com>
2021-08-28 16:58:54 -0300

    musica

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Three Days Grace - Animal I Have Become");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.WriteLine("I can't escape this hell");
+            Console.WriteLine("So many times I've tried");
+            Console.WriteLine("But I'm still caged inside");
+            Console.WriteLine("Somebody get me through this nightmare");
+            Console.WriteLine("I can't control myself");
+            Console.WriteLine("");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("So what if you can see the darkest side of me?");
+            Console.WriteLine("No one would ever change");
+            Console.WriteLine("This animal I have become");
+            Console.WriteLine("Help me believe it's not the real me");
+            Console.WriteLine("Somebody help me tame this animal");
+            Console.WriteLine("(This animal, this animal)");
+            Console.ResetColor();
+            Console.WriteLine("");
+
+            Console.WriteLine("I can't escape myself");
+            Console.WriteLine("(I can't escape myself)");
+            Console.WriteLine("So many times I've lied");
+            Console.WriteLine("(So many times I've lied)");
+            Console.WriteLine("But there's still rage inside");
+            Console.WriteLine("Somebody get me through this nightmare");
+            Console.WriteLine("I can't control myself");
+            Console.WriteLine("");
+
+            Console.WriteLine("So what if you can see the darkest side of me?");
+            Console.WriteLine("No one would ever change");
+            Console.WriteLine("This animal I have become");
+            Console.WriteLine("Help me believe it's not the real me");
+            Console.WriteLine("Somebody help me tame");
+            Console.WriteLine("This animal I have become");
+            Console.WriteLine("Help me believe it's not the real me");
+            Console.WriteLine("Somebody help me tame this animal");
+            Console.WriteLine("");
+
+            Console.WriteLine("Somebody help me through this nightmare");
+            Console.WriteLine("I can't control myself");
+            Console.WriteLine("Somebody wake me from this nightmare");
+            Console.WriteLine("I can't escape this hel");
+            Console.WriteLine("");
+            Console.WriteLine("This animal, this animal, this animal");
+            Console.WriteLine("");
+
+            Console.WriteLine("So what if you can see the darkest side of me?");
+            Console.WriteLine("No one will ever change");
+            Console.WriteLine("This animal I have become");
+            Console.WriteLine("Help me believe it's not the real me");
+            Console.WriteLine("Somebody help me tame");
+            Console.WriteLine("Help me believe it's not the real me");
+            Console.WriteLine("Somebody help me tame this animal");
+            Console.WriteLine("This animal I have become");
+
+
+
+
+
+        }
+    }
+}
